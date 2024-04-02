f0c48a6652cf4ea9a7ac7292a37e72a9b0145949
Mushahid-H <mushahid.bscssef20@iba-suk.edu.pk>
2024-04-02 23:26:02 +0500

    learnt mongodb installation and basic from connection to reading,creating and deleting model and schemas, lastly sessions and cookies handling

--- Express/proj1/routes/index.js
+++ Express/proj1/routes/index.js
@@ -1,9 +1,81 @@
-var express = require('express');
-var router = express.Router();
-
+var express = require('express')
+var router = express.Router()
+const userModel = require('./users')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+// learn sessions
+// req.session.give_any_name = 'any_value'
+router.get('/session', function (req, res) {
+  req.session.name = 'mushy'
+  req.session.ban = true
+  res.send('session set')
+})
+router.get('/sessionget', function (req, res) {
+  if (req.session.ban) {
+    res.send(` ${req.session.name} You have been banned from this website`)
+  } else {
+    res.render('index', { title: 'Mushahid' })
+  }
+})
+// delete session
+router.get('/sessiondelete', function (req, res) {
+  req.session.destroy(function (err) {
+    if (err) {
+      throw err
+    }
+  })
+  res.send('session deleted')
+})
+
+// create cookies
+
+router.get('/getcookie', function (req, res) {
+  res.cookie('name', 'mushy')
+  res.send('cookie set')
+})
+// read cookies
+router.get('/showcookie', function (req, res) {
+  res.send(req.cookies.name)
+})
+// delete cookie
+router.get('/deletecookie', function (req, res) {
+  res.clearCookie('name')
+  res.send('cookie deleted')
+})
+
+// create data
+router.get('/create', async function (req, res) {
+  const createUser = await userModel.create({
+    username: 'mushy',
+    password: 'mushtaq123',
+    age: 48,
+  })
+  res.send(createUser)
+})
+//  read/find all data
+router.get('/displayall', async (req, res) => {
+  const data = await userModel.find()
+
+  res.send(data)
+})
+// read one data
+router.get('/displayone', async (req, res) => {
+  const data = await userModel.findOne({ username: 'mushy' })
+  res.send(data)
+})
+
+// return deleted data
+router.get('/delete', async (req, res) => {
+  const data = await userModel.findOneAndDelete({ username: 'mushy' })
+  res.send(data)
+})
+
+// no return deleted data
+router.get('/delet', async (req, res) => {
+  const data = await userModel.deleteOne({ username: 'testme' })
+  res.send(data)
+})
+module.exports = router
